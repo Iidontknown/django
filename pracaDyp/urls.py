a24df316bd457178a56af1ca07517c4dc4c44bf8
Iidontknown <jakub-strugala@wp.pl>
2022-05-29 14:55:47 +0200

    konf

---
+++ pracaDyp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import front
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", front, name="front"),
+    path("ala/", front, name="front"),
+]
