cf1a0c845f3740c402833b7af8ceb306f96ca21e
Iidontknown <jakub-strugala@wp.pl>
2022-12-15 21:44:51 +0100

    asd

--- frontend/src/components/katalog/KatalogMenu.tsx
+++ frontend/src/components/katalog/KatalogMenu.tsx
@@ -24,21 +24,21 @@ const KatalogMenu: React.FC = () => {
      <Card>
     <Card.Img variant="top" height='160px' src="https://cdn.shopify.com/s/files/1/2222/6299/products/deutz1_245438b8-6fbf-4aef-955d-5c212c946e5a_1024x1024@2x.jpg?v=1527328553" />
     <Card.Body>
-      <Card.Title>kverneland pb 1xx 2001-2019</Card.Title>
+      <Card.Title>Przykładowa nazwa strony</Card.Title>
       <div className="text-right">1</div>
     </Card.Body>
   </Card> 
   <Card>
     <Card.Img variant="top" height='160px' src="https://cdn.shopify.com/s/files/1/2222/6299/products/deutz2_a89ea526-f40e-45c6-a2c8-ee3162ad49ac_800x.jpg?v=1527328553" />
     <Card.Body>
-      <Card.Title>kverneland pb 1xx 2001-2019</Card.Title>
+      <Card.Title>Lorem </Card.Title>
       <div className="text-right">2</div>
     </Card.Body>
   </Card>  
   <Card>
     <Card.Img variant="top" height='160px' src="https://image.slidesharecdn.com/deutzfahragrotron150tractorpartscataloguemanual-201227154807/85/deutz-fahr-agrotron-150-tractor-parts-catalogue-manual-14-320.jpg?cb=1668700405" />
     <Card.Body>
-      <Card.Title>kverneland pb 1xx 2001-2019</Card.Title>
+      <Card.Title>ipsum</Card.Title>
       <div className="text-right">3</div>
     </Card.Body>
   </Card> 
--- frontend/src/components/katalog/StronaKatalog.tsx
+++ frontend/src/components/katalog/StronaKatalog.tsx
@@ -1,22 +1,81 @@
-import * as React from 'react';
-import { useEffect } from 'react';
-import { Row, Form, Col, Button, Container, } from 'react-bootstrap';
+import * as React from "react";
+import { useEffect } from "react";
+import { Row, Form, Col, Button, Container, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const StronaKatalog: React.FC = () => {
-return(
-
+  return (
     <>
-    <Container>
+      <Container className="w-100 mh-100 p-20">
         <Row>
-    <Col>
-            asdas
-        </Col>
-        <Col>
-            asdas
-        </Col>
+          <Col className="w-50 mh-100">
+            <Image width={540} src="test.PNG"></Image>
+          </Col>
+          <Col className="w-50 mh-100 ">
+            <div className="overflow-scroll">
+              {" "}
+              <div className="p-2">
+                <Link to="/katalog">
+                  PrzykladowyProducent {">"}PrzykladowyModel {">"} Przykladowy
+                  Katalog
+                </Link>
+              </div>
+              <Row className="d-flex justify-content-between border-top p-2">
+                <Button>{"<"}</Button>
+                <p>Przykładowa Strona</p>
+                <Button>{">"}</Button>
+              </Row>
+              <div>
+              <Row className="border p-1  mt-2 d-flex flex-row">
+                  <div className="w-100">
+                    1-test
+                    <br />
+                    Opis: Brak
+                    <br/>
+                    Numer katalogowy: 312312
+                    <br />
+                    Numery pasujących cześci:
+                    <br />
+                    <ul>
+                      <li>m1234xszc - Przykładowy producent </li>
+                      <li>43565l - Przykładowy producent2 </li>
+                    </ul>
+                  </div>
+                  <div className="w-100 btn-group d-flex justify-content-center">
+                    <Button className="btn btn-success d-flex ">
+                      Dodaj do listy{" "}
+                    </Button>
+                  </div>
+                </Row>
+                <Row className="border p-1  mt-2 d-flex flex-row">
+                  <div className="w-100">
+                    2-Lorem
+                    <br />
+                    Opis: Przykładowy opis
+                    <br />
+                    Numer katalogowy: 12345
+                    <br />
+                    Numery pasujących cześci:
+                    <br />
+                    <ul>
+                      <li>5432 - Przykładowy producent </li>
+                    </ul>
+                  </div>
+                  <div className="w-100 btn-group d-flex justify-content-center">
+                    <Button className="btn btn-success d-flex ">
+                      Dodaj do listy{" "}
+                    </Button>
+                  </div>
+                </Row>
+        
+
+
+              </div>
+            </div>
+          </Col>
         </Row>
-    </Container>
+      </Container>
     </>
-)
-}
-export default StronaKatalog
+  );
+};
+export default StronaKatalog;
